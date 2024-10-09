0dfd421bc94bd1c78ef725c73498ca2df700c21e
joao_amoedo <jp.advc@gmail.com>
2024-10-09 07:20:03 -0300

    feat(visits_metrics) add visit_duration_seconds in visit_metrics

--- plugins/dbt_pg_project/models/2.gold/visits_metrics.sql
+++ plugins/dbt_pg_project/models/2.gold/visits_metrics.sql
@@ -1,18 +1,43 @@
-SELECT 
-    d.day,
-    COUNT(DISTINCT u.user_id) AS new_users_count,
-    COUNT(DISTINCT v.visitor_id) AS visitante_unico
-FROM 
-    dev_silver.dates d
-LEFT JOIN 
-    silver.users u 
-    ON d.day = CAST(u.created_at AS date)
-LEFT JOIN 
-    silver.visits v 
-    ON d.day = CAST(v.session_date AS date)
-WHERE 
-    d.day BETWEEN '2023-05-01' AND CURRENT_DATE
-GROUP BY 
-    d.day
-ORDER BY 
-    d.day
+
+WITH visit_counts AS (
+    SELECT
+        v.session_date AS date_day,
+        COUNT(DISTINCT v.visitor_id) AS visitor_count,
+        round(coalesce (AVG(CASE WHEN v.visit_duration_seconds > 0 THEN v.visit_duration_seconds END) / 60, 0),2) as visit_time_spend_in_minutes
+    FROM
+        {{ ref('visits') }} v
+    WHERE
+        v.session_date BETWEEN '2023-05-01' AND CURRENT_DATE
+    GROUP BY
+        v.session_date
+),
+user_counts AS (
+    SELECT
+        DATE(u.created_at) AS date_day,
+        COUNT(DISTINCT u.user_id) AS user_count
+    FROM
+        {{ ref('users') }} u
+    WHERE
+        u.created_at BETWEEN '2023-05-01' AND (CURRENT_DATE + INTERVAL '1 day')
+    GROUP BY
+        DATE(u.created_at)
+),
+dates AS (
+    SELECT
+        d.date_day
+    FROM
+        {{ ref('dates') }} d
+    WHERE
+        d.date_day BETWEEN '2023-05-01' AND CURRENT_DATE
+)
+SELECT
+    d.date_day,
+    COALESCE(vc.visitor_count, 0) AS visitor_count,
+    COALESCE(uc.user_count, 0) AS user_count,
+    coalesce(vc.visit_time_spend_in_minutes, 0) as visit_time_spend_in_minutes
+FROM
+    dates d
+    LEFT JOIN visit_counts vc ON vc.date_day = d.date_day
+    LEFT JOIN user_counts uc ON uc.date_day = d.date_day
+ORDER BY
+    d.date_day
