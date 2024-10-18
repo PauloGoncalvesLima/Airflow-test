28eab408b5beaca30e1473e1bbcbb94fee0d1cf6
Eric Silveira <silveirames@hotmail.com>
2024-10-18 11:17:01 -0300

    feat(bot-telegram): buscando os dados e atualizando o ultimo registro no banco

--- dags/processes_confs/dag_notify_telegram.py
+++ dags/processes_confs/dag_notify_telegram.py
@@ -41,24 +41,35 @@ def automation_notify_telegram():
         connection = hook.get_conn()
         postgres_cursor = connection.cursor()
 
-        count  = Variable.get(VARIABLE, default_var=0)
+        last_operation_count  = Variable.get(VARIABLE, default_var=0)
+
+        logging.info(f"O último elemento da operação anterior é: {last_operation_count}")
 
         query = f"""
             SELECT *
             FROM {SCHEMA}.{TABLE}
-            WHERE id > {count}
-            ORDER BY id DESC;
+            WHERE id > {last_operation_count}
+            ORDER BY id ASC;
         """
 
         postgres_cursor.execute(query)
         result = postgres_cursor.fetchall()
 
-        return result
+        last_id_query = result[-1][0]
+
+        logging.info(f"Query executada no banco, o último elemento atualmente é: {last_id_query}")
+
+        if last_id_query > last_operation_count:
+            Variable.set(VARIABLE, last_id_query)
+            return result
 
 
-    # @task(task_id='teste')
-    # def send_telegram_message():
+    # @task
+    # def send_telegram_message(ti=None):
+    #     data = ti.xcom_pull(task_ids='verify_atualization')
+
     #     telegram_hook = TelegramHook(telegram_conn_id=TELEGRAM_CONN_ID)
+
     #     telegram_hook.send_message(
     #         text="This is a test message from Airflow!",
     #         chat_id="<your_telegram_chat_id>",  # Add the chat ID here
@@ -66,6 +77,6 @@ def automation_notify_telegram():
 
 
 
-    start >> verify_atualization_db() >> end
+    start >> verify_atualization_db() >> send_telegram_message() >> end
 
 automation_notify_telegram()
