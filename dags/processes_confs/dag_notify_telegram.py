dc8ae481e5bc48bf5c4a01e9ced0e2663063fac7
Eric Silveira <silveirames@hotmail.com>
2024-10-23 15:59:12 -0300

    feat(bot-telegram): tratando os dados e as datas para envio

--- dags/processes_confs/dag_notify_telegram.py
+++ dags/processes_confs/dag_notify_telegram.py
@@ -69,7 +69,7 @@ def automation_notify_telegram():
 
 
     @task
-    def send_telegram_message(ti=None):
+    def transform_data(ti=None):
         if ti.xcom_pull(task_ids='verify_atualization') is None:
             logging.info(f"Não há novas atualizações!")
             return
@@ -100,12 +100,6 @@ def automation_notify_telegram():
         } for comp in participatory_processes}
 
         logging.info(f"Paticipatory processes id added!\n")
-
-        query_group_id = f"""
-            SELECT dc.id, dc.participatory_space_id
-            FROM decidim_components dc
-            WHERE dc.id IN ({','.join(map(str, components_proposals))});
-        """
         
         cursor_postgres.execute(query)
         participatory_processes = cursor_postgres.fetchall()
@@ -123,7 +117,7 @@ def automation_notify_telegram():
                     "decidim_participatory_processes": participatory_data["participatory_process_id"],
                     "decidim_participatory_process_group_id": participatory_data["participatory_process_group_id"],
                     "decidim_scope_id": values_data[2],
-                    "created_at": values_data[3],
+                    "created_at": values_data[3].strftime("%d/%m/%Y %H:%M:%S"),
                     "updated_at": values_data[4],
                     "proposal_votes_count": values_data[5],
                     "state": values_data[6],
@@ -133,16 +127,59 @@ def automation_notify_telegram():
                     "address": values_data[10],
                     "latitude": values_data[11],
                     "longitude": values_data[12],
-                    "published_at": values_data[13],
+                    "published_at": values_data[13].strftime("%d/%m/%Y %H:%M:%S"),
                     "proposal_notes_count": values_data[14],
                     "coauthorships_count": values_data[15],
-                    "participatory_text_level": values_data[16]
+                    "participatory_text_level": values_data[16],
+                    "position": values_data[17],
+                    "created_in_meeting": values_data[18],
+                    "cost": values_data[19],
+                    "cost_report": values_data[20],
+                    "execution_period": values_data[21],
+                    "state_published_at": values_data[22],
+                    "endorsements_count": values_data[23],
+                    "title": values_data[24],
+                    "body": values_data[25],
+                    "comments_count": values_data[26],
+                    "follows_count": values_data[27],
+                    "is_interactive": values_data[28],
+                    "badge_array": values_data[29] 
                 }
                 json_list.append(data_json)
+                logging.info(f"JSON: {data_json}")
 
         logging.info(f"JSON gerado com sucesso: {json_list}")
+        return json_list
 
 
+    @task(task_id='sending_messages')
+    def sending_messages(ti=None):
+
+        json_data = ti.xcom_pull(task_ids='transform_data')
+
+        logging.info(f"JSON gerado com sucesso: {json_data}")
+
+        state_map = {
+        "accepted": {"label": "aceita ", "emoji": "✅ ✅ ✅"},
+        "evaluating": {"label": "em avaliação ", "emoji": "📥 📥 📥"},
+        "withdrawn": {"label": "retirada ", "emoji": "🚫 🚫 🚫"},
+        "rejected": {"label": "rejeitada ", "emoji": "⛔️ ⛔️ ⛔️"},
+        "others": {"label": "atualizada ", "emoji": "🔄 🔄 🔄"},
+        "new": {"label": "", "emoji": "📣 📣 📣 <b>[NOVA]</b>"},
+        }
+
+        message =[]
+
+        for value in json_data:
+            for label, characteristics in state_map.items():
+                if label in value['state']:
+                    message.append(f"{characteristics['emoji']} Proposta <b>{characteristics['label']}</b>em {value['published_at']}\n\n")
+                    f"<b>Proposta</b>\n{value['title']}\n\n"
+                    f"<b>Autor</b>\n{value['aut']} {organization_name}\n\n"
+                    f"<b>Categoria</b>\n{data_row['category']}\n\n"
+                    f"<b>Descrição</b>\n{value['body']}\n\n")
+
+        logging.info(f"Imprimindo vetor de mensagens: {message}")
 
         telegram_hook = TelegramHook(telegram_conn_id=TELEGRAM_CONN_ID)
 
@@ -155,6 +192,6 @@ def automation_notify_telegram():
 
 
 
-    start >> verify_atualization_db() >> send_telegram_message() >> end
+    start >> verify_atualization_db() >> transform_data() >> sending_messages() >> end
 
 automation_notify_telegram()
